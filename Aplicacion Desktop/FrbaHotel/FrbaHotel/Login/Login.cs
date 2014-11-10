c6acaa03a8188c1dc1ae298a5abcaa42d80e1cf8
cchibana <chibana.cynthia@gmail.com>
2014-11-10 18:03:07 -0300

    app1.config

--- Aplicacion Desktop/FrbaHotel/FrbaHotel/Login/Login.cs
+++ Aplicacion Desktop/FrbaHotel/FrbaHotel/Login/Login.cs
@@ -24,24 +24,25 @@ namespace FrbaHotel.Login
             //Generar MenuStrip para la pantalla inicial según el rol del usuario
             PantallaPrincipal pantPrinc;
 
-            Contrasenia.encriptarContrasenia contraseniaUsr = new Contrasenia.encriptarContrasenia();
-            string hashContr = contraseniaUsr.hash(txt_passwordUsuario.Text);
+            //Contrasenia.encriptarContrasenia contraseniaUsr = new Contrasenia.encriptarContrasenia();
+            //string hashContr = contraseniaUsr.hash(txt_passwordUsuario.Text);
 
-            if (txt_nombreUsuario.Text == "admin" && hashContr == "e6b87050bfcb8143fcb8db0170a4dc9ed00d904ddd3e2a4ad1b1e8dc0fdc9be7")
-            {
-                this.Hide();
-                //Pantalla Administrador
-                pantPrinc = new PantallaPrincipal("administrador");
-                pantPrinc.Show(this);
-            }
-            else if (txt_nombreUsuario.Text == "recepcionista" && hashContr == "bd2f76155a54ecf99bd3efd53dfbadf54d7b0ecd7b99f989449dfb817c0bb744")
-            {
-                this.Hide();
-                //Pantalla Recepcionista
-                //recepcionista -> usuario: recepcionista, contraseña recepcionista
-                pantPrinc = new PantallaPrincipal("recepcionista");
-                pantPrinc.Show(this);
-            }
+
+            //if (txt_nombreUsuario.Text == "admin" && hashContr == "e6b87050bfcb8143fcb8db0170a4dc9ed00d904ddd3e2a4ad1b1e8dc0fdc9be7")
+            //{
+            //    this.Hide();
+            //    //Pantalla Administrador
+            //    pantPrinc = new PantallaPrincipal("administrador");
+            //    pantPrinc.Show(this);
+            //}
+            //else if (txt_nombreUsuario.Text == "recepcionista" && hashContr == "bd2f76155a54ecf99bd3efd53dfbadf54d7b0ecd7b99f989449dfb817c0bb744")
+            //{
+            //    this.Hide(); 
+            //    //Pantalla Recepcionista
+            //    //recepcionista -> usuario: recepcionista, contraseña recepcionista
+            //    pantPrinc = new PantallaPrincipal("recepcionista");
+            //    pantPrinc.Show(this);
+            //}
         }
 
         private void btn_salir_Click(object sender, EventArgs e)
